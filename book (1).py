7f82e1e2334f69dbfa1f7be318d321df7c2ac2d4
amit5570 <44551103+amit5570@users.noreply.github.com>
2020-02-17 13:17:09 +0530

    Add files via upload

---
+++ book (1).py
@@ -0,0 +1,26 @@
+class Book:
+    
+    def accept_input(self):                   
+        self.name = str(input("Enter name of Book: "))
+        self.author = str(input("Enter author of Book: "))
+        self.price = float(input("Enter price of Book: "))
+        self.n_copies = int(input("Enter no. of copies to purchase of Book: "))
+
+    def total_price(self):                    
+        t = self.price*self.n_copies
+        print("Total Price: ", t)
+
+    def purchase_details(self):
+        print("Purcashe Details".center(70,"-"))
+        print("Name of Book : ", self.name)
+        print("Author of Book : ", self.author)
+        print("Price of Book : ", self.price)
+        print("Number of copies : ", self.n_copies)
+
+
+
+obj=Book()
+
+obj.accept_input()
+obj.purchase_details()
+obj.total_price()
